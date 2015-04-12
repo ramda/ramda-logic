73cc94e7797763d7ff6c5675b2ef4432646cac6b
buzzdecafe <m_hur@yahoo.com>
2015-04-12 11:52:14 -0400

    another simple fresh test

--- test/fresh.js
+++ test/fresh.js
@@ -7,6 +7,10 @@ var unify = require('../src/unify');
 describe('fresh', function() {
   function q5(q) { return unify(q, 5); }
   it('returns a new goal function', function() {
+    expect(fresh(q5)).to.be.a('function');
+  });
+
+  it('creates a new logic variable in the returned state', function() {
     expect(fresh(q5)(smap({}))).to.eql(succeed({'_.0': 5}));
   });
 });
