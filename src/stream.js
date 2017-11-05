3c82bafc91abb78c57f2d0a26762ebd439609c2a
Michael Hurley <mh@buzzdecafe.com>
2017-11-05 18:35:29 -0500

    uniion types are beautiful

--- src/stream.js
+++ src/stream.js
@@ -10,21 +10,21 @@ const Stream = Type({
 Stream.prototype = Object.freeze({
   chain: function(f) {
     return Stream.case({
-      Empty: () => Stream.Empty,
+      Empty: ()           => Stream.Empty,
       Cons:  (head, tail) => f(head).concat(tail().chain(f))
     }, this);
   },
 
   concat: function(s) {
     return Stream.case({
-      Empty: () => s,
+      Empty: ()           => s,
       Cons:  (head, tail) => Stream.Cons(head, () => tail().concat(s))
     }, this);
   },
 
   head: function() {
     return Stream.case({
-      Empty: () => Stream.Empty,
+      Empty: ()        => Stream.Empty,
       Cons:  (head, _) => head
     }, this);
   },
@@ -40,30 +40,39 @@ Stream.prototype = Object.freeze({
 
   map: function(f) {
     return Stream.case({
-      Empty: () => Stream.Empty,
+      Empty: ()           => Stream.Empty,
       Cons:  (head, tail) => Stream.Cons(f(head), () => tail().map(f))
     }, this);
   },
 
-  of: x => Stream.Cons(x, () => Stream.Empty),
-
   tail: function() {
     return Stream.case({
-      Empty: () => Stream.Empty,
+      Empty: ()        => Stream.Empty,
       Cons:  (_, tail) => tail()
     }, this);
   },
 
   toArray: function() {
     return Stream.case({
-      Empty: () => [],
+      Empty: ()           => [],
       Cons:  (head, tail) => [head].concat(tail().toArray())
+    }, this);
+  },
+
+  toString: function() {
+    return Stream.case({
+      Empty: () => 'Empty',
+      Cons: (head, tail) => `Cons(${head}, ${tail().toString()})`
     }, this);
   }
 });
 
 Stream.empty = () => Stream.Empty;
 
+Stream.fromArray = xs => xs.reduceRight((acc, x) => Stream.Cons(x, () => acc), Stream.Empty);
+
 Stream.isStream = s => s && s.isStream === Stream.prototype.isStream && s.isStream();
+
+Stream.of = x => Stream.Cons(x, () => Stream.Empty);
 
 export default Stream;
