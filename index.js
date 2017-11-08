0b3cf3642682ff3139f5c134e599bf47f47449c8
Michael Hurley <mh@buzzdecafe.com>
2017-11-07 20:14:06 -0500

    export stream

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import conj from './src/conj';
 import disj from './src/disj';
 import fail from './src/fail';
 import fresh from './src/fresh';
+import Stream from './src/stream';
 import succeed from './src/succeed';
 import unify from './src/unify';
 
-export default { conj, disj, fail, fresh, succeed, unify };
+export default { conj, disj, fail, fresh, Stream, succeed, unify };
